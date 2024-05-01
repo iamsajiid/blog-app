020532dcd734c2a460435dfbfaa1b2048b3eb3ba
sajid <iamsajiid@gmail.com>
2024-05-01 21:10:50 +0530

    added storage service

---
+++ src/services/auth.js
@@ -0,0 +1,55 @@
+import { Client, Account, ID } from "appwrite";
+import { appwriteURL, appwriteProjectID } from "../config/config"
+
+class AuthService {
+    client = new Client()
+    account
+
+    constructor(){
+        this.client.setEndpoint(appwriteURL).setProject(appwriteProjectID)
+        this.account = new Account(this.client)
+    }
+
+    createUser = async ({email, password, name}) => {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if(userAccount){
+                this.login(email, password)
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            console.log("Appwrite Error :: Create User :: ", error)
+        }
+    }
+
+    login = async (email, password) => {
+        try {
+            return await this.account.createEmailPasswordSession(email, password)
+        } catch (error) {
+            console.log("Appwrite Error :: Login :: ", error)
+            return false;
+        }
+    }
+
+    getCurrentUser = async () => {
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("Appwrite Error :: Current User :: ", error)
+        }
+        return null;
+    }
+
+    logOut = async () => {
+        try {
+            return await this.account.deleteSession("current")
+        } catch (error) {
+            console.log("Appwrite Error :: Current User :: ", error)
+        }
+    }
+}
+
+const authService = new AuthService()
+
+export default authService
---
+++ src/services/storage.js
@@ -0,0 +1,118 @@
+import {Client, Databases, Query, Storage, ID} from 'appwrite'
+import config from "../config/config"
+
+class StorageService {
+    client = new Client()
+    databases
+    bucket
+
+    constructor() {
+        this.client.setEndpoint(config.appwriteURL).setProject(config.appwriteProjectID)
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
+    }
+
+    createDocument = async ({title, slug, content, featuredImage, status, userID}) => {
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseID,
+                config.appwriteCollectionID,
+                slug,
+                {
+                    userID,
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite Error :: createDocument :: ", error)
+        }
+    }
+
+    updateDocument = async (slug, {title, content, featuredImage, status}) => {
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseID,
+                config.appwriteCollectionID,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite Error :: updateDocument :: ", error)
+        }
+    }
+
+    deleteDocument = async (slug) => {
+        try {
+            return this.databases.deleteDocument(config.appwriteDatabaseID, config.appwriteCollectionID, slug)
+        } catch (error) {
+            console.log("Appwrite Error :: deleteDocument :: ", error)
+            return false;
+        }
+    }
+
+    getDocument = async (slug) => {
+        try {
+            return this.databases.getDocument(config.appwriteDatabaseID, config.appwriteCollectionID, slug)
+        } catch (error) {
+            console.log("Appwrite Error :: getDocument :: ", error)
+            return false;
+        }
+    }
+
+    getDocumentUsingQueries = async (queries = Query.equal("status", "active")) => {
+        try {
+            this.databases.listDocuments(
+                config.appwriteDatabaseID,
+                config.appwriteCollectionID,
+                queries
+            )
+        } catch (error) {
+            console.log("Appwrite Error :: getDocumentUsingQueries :: ", error)
+            return false;
+        }
+    }
+
+    uploadFile = async (file) => {
+        try {
+            return await this.bucket.createFile(
+                config.appwriteBucketID,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite Error :: uploadFile :: ", error)
+        }
+    }
+
+    deleteFile = async (fileID) => {
+        try {
+            await this.bucket.deleteFile(
+                config.appwriteBucketID,
+                fileID
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite Error :: deleteFile :: ", error)
+            return false;
+        }
+    }
+
+    getFilePreview = (fileID) => {
+        try {
+            return this.bucket.getFilePreview(
+                config.appwriteBucketID,
+                fileID
+            )
+        } catch (error) {
+            console.log("Appwrite Error :: getFilePreview :: ", error)
+        }
+    }
+}
